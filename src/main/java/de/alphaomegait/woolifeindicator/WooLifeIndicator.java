9ab076402d7059ec9d41a826ba3a83cf69248bcf
BuildTools <justin.eiletz@alphaomega-it.de>
2024-02-25 09:02:40 +0100

    Removing unnecessary boilerplate code

--- src/main/java/de/alphaomegait/woolifeindicator/WooLifeIndicator.java
+++ src/main/java/de/alphaomegait/woolifeindicator/WooLifeIndicator.java
@@ -6,15 +6,14 @@ import de.alphaomegait.woolifeindicator.listener.OnScoreboardCheck;
 import lombok.Getter;
 import me.blvckbytes.autowirer.AutoWirer;
 import me.blvckbytes.bukkitboilerplate.PluginFileHandler;
-import me.blvckbytes.bukkitcommands.BukkitCommand;
 import me.blvckbytes.bukkitevaluable.ConfigManager;
 import me.blvckbytes.bukkitevaluable.IConfigPathsProvider;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
-import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -59,6 +58,21 @@ public class WooLifeIndicator extends JavaPlugin implements IConfigPathsProvider
 			.addSingleton(LifeIndicator.class)
 			.addSingleton(DamageIndicatorListener.class)
 			.addSingleton(OnScoreboardCheck.class)
+			.addInstantiationListener(
+				Listener.class,
+				(listener, dependencies) -> Bukkit.getPluginManager().registerEvents(
+					listener,
+					this
+				)
+			)
+			.addInstantiationListener(
+				Command.class,
+				(command, dependencies) -> Bukkit.getCommandMap()
+																			 .register(
+								command.getName(),
+								command
+							)
+			)
 			.onException(exception -> {
 				// Log exception and disable plugin
 				this.logger.log(
@@ -75,17 +89,6 @@ public class WooLifeIndicator extends JavaPlugin implements IConfigPathsProvider
 					"Successfully loaded " + success.getInstancesCount() + " classes (" + ((System.nanoTime() - beginTimestamp) / 1000 / 1000) + "ms)"
 				);
 			});
-
-		// Register commands
-		this.registerCommands(
-			this.autoWirer.findInstance(LifeIndicator.class)
-		);
-
-		// Register listeners
-		this.registerListener(
-			this.autoWirer.findInstance(DamageIndicatorListener.class),
-			this.autoWirer.findInstance(OnScoreboardCheck.class)
-		);
 	}
 
 	@Override
@@ -105,57 +108,5 @@ public class WooLifeIndicator extends JavaPlugin implements IConfigPathsProvider
 			"commands/life-indicator-config.yml",
 			"utilities/damage-indicator-config.yml"
 		};
-	}
-
-	/**
-	 * Register a list of Bukkit commands.
-	 *
-	 * @param  bukkitCommands  list of optional Bukkit commands
-	 */
-	@SafeVarargs
-	private void registerCommands(
-		final Optional<? extends BukkitCommand>... bukkitCommands
-	) {
-		for (final Optional<? extends BukkitCommand> command : bukkitCommands)
-			command.ifPresentOrElse(
-				bukkitCommand ->
-					this.getServer()
-					.getCommandMap()
-					.register(
-						bukkitCommand.getName(),
-						bukkitCommand
-					)
-				,
-				() -> this.logger.info(
-					"Command: '" +
-					command.getClass() +
-					"' could not be registered. (" +
-					(
-						command.map(Command::getName).orElse("")
-					) +
-					")"
-				)
-			);
-	}
-
-	/**
-	 * Registers the given Bukkit listeners with the plugin manager.
-	 *
-	 * @param  bukkitListeners  an optional array of Bukkit listeners to register
-	 */
-	@SafeVarargs
-	private void registerListener(
-		final Optional<? extends Listener>... bukkitListeners
-	) {
-		for (final Optional<? extends Listener> listener : bukkitListeners)
-			listener.ifPresentOrElse(
-				bukkitListener -> this.getServer()
-															.getPluginManager()
-															.registerEvents(
-																bukkitListener,
-																this
-															),
-				() -> this.logger.info("Listener: '" + listener.getClass() + "' could not be registered.")
-			);
 	}
 }
